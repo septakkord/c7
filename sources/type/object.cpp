dd0f28659449afea3eac4449f2198645f39a2fcd
septakkord <vladimir@septima.org>
2016-02-07 10:59:05 +0300

    Clone and destructor added into c7.

--- sources/type/object.cpp
+++ sources/type/object.cpp
@@ -1,9 +1,30 @@
 #include "object/scalar_holder"
+#include <new>
 
 namespace type
 {
     object::object()
         : m_holder()
     {
+    }
+
+    object::~object()
+    {
+        if (m_holder)
+            m_holder->~holder();
+    }
+
+    object object::clone() const
+    {
+        object result;
+        if (m_holder)
+            m_holder->clone(result.m_buffer);
+        return result;
+    }
+
+    template <>
+    void object::set_as(const std::int64_t& value)
+    {
+        m_holder = new(m_buffer) scalar_holder<std::int64_t>(value);
     }
 }
