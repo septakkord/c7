8b8ffb51c0d321316039af69003288a4768a019c
septakkord <vladimir@septima.org>
2016-02-07 11:18:57 +0300

    c7 include moved to c7 directory in root.

--- sources/type/object.cpp
+++ sources/type/object.cpp
@@ -1,30 +1,33 @@
 #include "object/scalar_holder"
 #include <new>
 
-namespace type
+namespace c7
 {
-    object::object()
-        : m_holder()
+    namespace type
     {
-    }
+        object::object()
+            : m_holder()
+        {
+        }
 
-    object::~object()
-    {
-        if (m_holder)
-            m_holder->~holder();
-    }
+        object::~object()
+        {
+            if (m_holder)
+                m_holder->~holder();
+        }
 
-    object object::clone() const
-    {
-        object result;
-        if (m_holder)
-            m_holder->clone(result.m_buffer);
-        return result;
-    }
+        object object::clone() const
+        {
+            object result;
+            if (m_holder)
+                m_holder->clone(result.m_buffer);
+            return result;
+        }
 
-    template <>
-    void object::set_as(const std::int64_t& value)
-    {
-        m_holder = new(m_buffer) scalar_holder<std::int64_t>(value);
+        template <>
+        void object::set_as(const std::int64_t& value)
+        {
+            m_holder = new(m_buffer)scalar_holder<std::int64_t>(value);
+        }
     }
 }
